9b6b188889caf9c6292610d06da7ddc8c108a4b3
cipreses <cipreses@gmail.com>
2025-11-08 09:02:40 -0300

    Update posiciona4Servos.ino
    Agrega los servolocos

--- posiciona4Servos.ino
+++ posiciona4Servos.ino
@@ -1,8 +1,7 @@
 
-
 #include <Servo.h>
 
-Servo Lefttop,Righttop,Leftbottom,Rightbottom;
+Servo Lefttop,Righttop,Leftbottom,Rightbottom, Servoloco1, Servoloco2;
 
 // Face should be in front of you
 /*
@@ -14,9 +13,13 @@ Lefttop =>      |               |    <= Righttop
                     ||     ||
 Leftbottom =>     -----   ------     <= Rightbottom
                  |-----   ------|
+
+
+Servoloco 1 y servoloco 2 sirven para mover servos en todo su recorrido y probar movimienros                  
 */
 
 int pos = 90;    // variable to store the servo position
+int posloco = 0;
 //make sure all motors are at 90 degree
 
 
@@ -25,6 +28,9 @@ void setup() {
   Righttop.attach(9);  // attaches the servo on pin 9 to the servo object
   Leftbottom.attach(10);  // attaches the servo on pin 10 to the servo object
   Rightbottom.attach(11);  // attaches the servo on pin 11 to the servo object
+  Servoloco1.attach(12); //Lo mueve como loco.
+  Servoloco2.attach(13); //Lo mueve como loco.
+
 }
 
 void loop() {
@@ -32,5 +38,19 @@ void loop() {
     Lefttop.write(pos);           
     Righttop.write(pos);              
     Leftbottom.write(pos);              
-    Rightbottom.write(pos);              
+    Rightbottom.write(pos);  
+
+  for (posloco = 0; posloco <= 180; posloco += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    Servoloco1.write(posloco);              // tell servo to go to position in variable 'pos'
+    Servoloco2.write(posloco);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+  for (posloco = 180; posloco >= 0; posloco -= 1) { // goes from 180 degrees to 0 degrees
+    Servoloco1.write(posloco);              // tell servo to go to position in variable 'pos'
+    Servoloco2.write(posloco);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+
+
 }
